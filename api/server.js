1aa568df9c041d3f31cfccc52f89f0047c2286ca
atolmie <atolmie22@gmail.com>
2019-07-29 13:12:24 -0500

    config express session

--- api/server.js
+++ api/server.js
@@ -3,7 +3,23 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const cors = require('cors')
 
+const session = require('express session') 
+
+//config express-session 
+const sess = {
+secret: 'CHANGE THIS TO A RANDOM SECRET',
+cookie: {},
+resave: false,
+saveUninitialized: true};
+
+if (app.get('env') === 'production') {
+    sess.cookie.secure = true; // serve secure cookies, requires https
+  }
+  
+  app.use(session(sess));
+
 const server = express()
+
 
 server.use(express.json())
 server.use(helmet())
