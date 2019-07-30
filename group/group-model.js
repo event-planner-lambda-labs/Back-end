48bf9202951bcbe85addf473ed37e2f48e784871
atolmie <atolmie22@gmail.com>
2019-07-30 17:00:45 -0500

    creates group model

--- group/group-model.js
+++ group/group-model.js
@@ -1,1 +1,51 @@
 const db = require("../database/dbConfig")
+
+module.exports = {
+    getGroupMembers,
+    add,
+    find,
+    findById,
+    update,
+    remove,
+    
+}
+
+function find() {
+  return db("group").select("id", "name")
+}
+
+function getGroupMembers(id) {
+    return db("group")
+    .join("member", "group.member_id", "member.id")
+    .where("group.id",id)
+    .first()
+}
+
+function findById(id) {
+    return db("group")
+    .select("id", "name")
+    .where({id})
+    .first()
+}
+
+function add(group) {
+    const [id] = await db("group").insert(group)
+    return findById(id);
+}
+
+function update(id, changes) {
+    return db("group")
+    .where({id})
+    .update(changes)
+    .then(()=>{
+        return db("group")
+        .where({id})
+        .first()
+    })
+}
+
+function remove(id) {
+    return db("group")
+    .where({id})
+    .del()
+}
