5919c0ed6d03158aca6824c6ae9bc4995372702e
atolmie <atolmie22@gmail.com>
2019-07-31 12:42:21 -0500

    creates group route

--- group/group-router.js
+++ group/group-router.js
@@ -1,1 +1,81 @@
 const router = require("express").Router()
+
+const Groups = require("./group-model");
+
+// Get
+
+router.get("/", (req, res) => {
+  Groups.find()
+    .then(groups => {
+      res.status(200).json(groups);
+    })
+    .catch(error => res.status(500).json(error));
+});
+// Get Group Members
+
+router.get("/:id/members", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const groupMembers = await Groups.getGroupMembers(id);
+
+        res.status(200).json(groupMembers); 
+    } catch ({ message }) {
+        console.log({ message });
+        res.status(500).json({ message: "server error finding members" });
+      }
+})
+
+// Get by ID
+
+router.get("/:id", (req, res) => {
+  Groups.findById()
+    .then(groups => {
+      res.status(200).json(groups);
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+// Post
+
+router.post("/", (req, res) => {
+  Groups.add()
+    .then(groups => {
+      res.status(201).json(groups);
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+// Put
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updateHost = await Groups.update(req.params.id, req.body);
+    if (updateHost) {
+      res.status(200).json(updateHost);
+    } else {
+      res.status(404).json({ message: "group not found" });
+    }
+  } catch ({ message }) {
+    console.log({ message });
+    res.status(500).json({ message: "server error updating" });
+  }
+});
+
+// Delete
+
+router.delete("/:id", (req, res) => {
+  Groups.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "group deleted" });
+      } else {
+        res.status(404).json({ message: "group not found" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "server error deleting group" });
+    });
+});
+
+module.exports = router;
