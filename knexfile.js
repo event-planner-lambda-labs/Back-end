0734f4bf53da9cb80eeb35fe48ea0c1c95edeb5f
Oliver-Strange <preston.burton@gmail.com>
2019-08-06 10:34:39 -0700

    sets up knexfile for heroku

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,8 @@
 // Update with your config settings.
+require("dotenv").config();
+const pg = require("pg");
+pg.defaults.ssl = true;
+
 const dbConnection = process.env.DATABASE_URL;
 const dbPassword = process.env.DB_PASSWORD;
 
@@ -26,7 +30,7 @@ module.exports = {
     connection: dbConnection,
     migrations: {
       tableName: "knex_migrations",
-      directory: ".database/migrations"
+      directory: "./database/migrations"
     },
     seeds: {
       directory: ".database/seeds"
